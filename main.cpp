f58a65fd87e5a4a240bb94b9c51c41e351ec3fa5
warp-static <rdorman34@gmail.com>
2018-09-28 11:40:37 +0100

    added opengl context

--- main.cpp
+++ main.cpp
@@ -10,18 +10,18 @@ int main()
 
     SDL_Window* window = NULL;
 
-    window = SDL_CreateWindow("Disaster", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1187, 627, SDL_WINDOW_RESIZABLE /*| SDL_WINDOW_BORDERLESS*/);
+    window = SDL_CreateWindow("Disaster", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1187, 627, SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL /*| SDL_WINDOW_BORDERLESS*/);
 
     if(!window)
     {
         std::cout << "failed to create SDL window" << std::endl << SDL_GetError() << std::endl;
     }
 
-    SDL_Surface* screen = SDL_GetWindowSurface(window);
-    unsigned int red = SDL_MapRGB(screen->format, 100, 0, 0);
-    SDL_FillRect( screen, NULL, red);
+    GLenum res = glewInit();
 
-    SDL_UpdateWindowSurface(window);
+    SDL_GLContext glcontext = SDL_GL_CreateContext(window);
+
+    glClearColor(100, 0, 0, 1);
 
     unsigned int starting_tick;
     bool running = true;
@@ -38,6 +38,8 @@ int main()
                 running = false;
                 break;
             }
+            glClear(GL_COLOR_BUFFER_BIT);
+            SDL_GL_SwapWindow(window);
         }
 
         if((1000 / fps) > SDL_GetTicks() - starting_tick)
@@ -45,6 +47,8 @@ int main()
             SDL_Delay(1000 / fps - (SDL_GetTicks() - starting_tick));
         }
     }
+
+    SDL_GL_DeleteContext(glcontext);
 
     SDL_DestroyWindow(window);
 
